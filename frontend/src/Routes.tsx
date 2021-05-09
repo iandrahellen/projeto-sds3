a9f7108f83d69e9fa8545f17b1fa74fce5338c95
Iandra Hellen <iandrahellenmorais@gmail.com>
2021-05-09 18:51:17 -0300

    First request

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,22 +1,20 @@
-import Dashboard from 'pages/Dashboard';
-import Home from 'pages/Home';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from "pages/Dashboard";
+import Home from "pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const Routes = () => {
     return (
-        <BrowserRouter>
-            <switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/dashboard">
-                    <Dashboard />
-                </Route>
-            </switch>
-
-        </BrowserRouter>
-
+      <BrowserRouter>
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/dashboard">
+                <Dashboard />
+            </Route>
+        </Switch>
+      </BrowserRouter>
     );
-}
-
-export default Routes;
+  };
+  
+  export default Routes;
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,25 +1,44 @@
-import Chart from 'react-apexcharts';
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSum } from "types/sale";
+
+type ChartData = {
+  series: number[];
+  labels: string[];
+};
 
 const DonutChart = () => {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
-    const options = {
-        legend: {
-            show: true
-        }
-    }
+  const [chartData, setChartData] = useState<ChartData>({
+    series: [],
+    labels: [],
+  });
 
-    return (
-        <Chart
-            options={{ ...options, labels: mockData.labels }}
-            series={mockData.series}
-            type="donut"
-            height="240"
-        />
-    );
-}
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
+
+      setChartData({ series: mySeries, labels: myLabels });      
+    });
+  }, []);
+
+  const options = {
+    legend: {
+      show: true,
+    },
+  };
+
+  return (
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
+      type="donut"
+      height="240"
+    />
+  );
+};
 
 export default DonutChart;
